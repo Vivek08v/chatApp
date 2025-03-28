adf947e39852fe515cf2401c5ace1051c5471ca3
Vivek08v <vivekkumar08v@gmail.com>
2025-03-28 19:49:35 +0530

    signup controller done

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,5 +1,70 @@
-export const signup = (req, res) => {
-    res.send("signIn route")
+import { generateToken } from "../lib/utils.js";
+import { User } from "../models/user.model.js";
+import bcrypt from "bcryptjs"
+
+export const signup = async(req, res) => {
+    // res.send("signIn route")
+    try{
+        const {fullName, email, password} = req.body;  // becoz of app.use(express.json())
+
+        if(!fullName || !email || !password){
+            return res.status(400).json({
+                success: false,
+                message: "All fields are required"
+            })
+        }
+
+        const user = await User.findOne({email});
+
+        if(user){
+            return res.status(400).json({
+                success: false,
+                message: "User already exists"
+            })
+        }
+
+        if(password.length < 6){
+            return res.status(400).json({
+                success: false,
+                message: "Password must be at least 6 characters"
+            })
+        }
+
+        // hash the password
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        const newUser = new User({
+            fullName: fullName,
+            email: email,
+            password: hashedPassword,
+        })
+
+        if(newUser){
+            // generate jwt token here
+            generateToken(newUser._id, res);
+            const storedUser = await newUser.save();
+
+            return res.status(201).json({
+                success: true,
+                data: storedUser,
+                message: "User added Successfully"
+            })
+        }
+        else{
+            return res.status(400).json({
+                success: false,
+                message: "Invalid user data"
+            })
+        }
+    }
+    catch(error){
+        console.log("Error in signup controller", error.message);
+        return res.status(400).json({
+            success: false,
+            message: "Password mus be at least 6 characters"
+        })
+    }
 }
 
 export const login = (req, res) => {
---
+++ backend/src/lib/utils.js
@@ -0,0 +1,18 @@
+import { configDotenv } from "dotenv";
+import jwt from "jsonwebtoken";
+
+export const generateToken = (userId, res) =>{
+
+    const token = jwt.sign({userId}, process.env.JWT_SECRET, {
+        expiresIn:"7h"
+    })
+
+    res.cookie("jwt", token, {
+        maxAge: 7*24*60*60*1000, //in ms
+        httpOnly: true,   // prevents XSS attacks cross-site scripting attacks
+        sameSite: "strict",  //CSRF attacks cross-site request forgery attacks
+        secure: process.env.NODE_ENV !== "development"
+    })
+
+    return token;
+}
