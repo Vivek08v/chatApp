94158b457f20e99f9cdc60ef5cd94f6c71b3ec9e
Vivek08v <vivekkumar08v@gmail.com>
2025-03-28 23:18:24 +0530

    authN, protectRoute Done

---
+++ backend/src/middleware/auth.middleware.js
@@ -0,0 +1,44 @@
+import jwt from "jsonwebtoken";
+import { User } from "../models/user.model";
+
+export const protectRoute = async(req, res, next) => {
+    try{
+        const token = req.cookies.jwt;  // jwt is token name
+
+        if(!token){
+            return res.status(400).json({
+                success: "false",
+                message: "Unauthorized - No Token Provided"
+            })
+        }
+
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+        if(!decoded){
+            return res.status(400).json({
+                success: "false",
+                message: "Unauthorized - Incorrect Token"
+            })
+        }
+
+        const user = await User.findById(decoded.userId).select("-password");
+
+        if(!user){
+            return res.status(400).json({
+                success: "false",
+                message: "Unauthorized - No Token Provided"
+            })
+        }
+
+        req.user = user;
+
+        next();
+    }
+    catch(error){
+        console.log("Error in protect route", error)
+        return res.status(400).json({
+            success: "true",
+            message: "Something went wrong in protect route"
+        })
+    }
+}
