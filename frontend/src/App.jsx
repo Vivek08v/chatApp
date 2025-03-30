e56df42418d0b921d668e172abd36dd63751a1be
Vivek08v <vivekkumar08v@gmail.com>
2025-03-30 10:40:19 +0530

    axios, zustand and basic routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,4 @@
 import './App.css'
-import { useState } from 'react'
-import { Route, Routes } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import HomePage from './pages/HomePage'
 import SignUpPage from './pages/SignUpPage'
@@ -8,9 +6,26 @@ import LoginPage from './pages/LoginPage'
 import SettingsPage from './pages/SettingsPage'
 import ProfilePage from './pages/ProfilePage'
 
+import { useEffect, useState } from 'react'
+import { Navigate, Route, Routes } from 'react-router-dom'
+import { useAuthStore } from './store/useAuthStore'
+import { Loader } from 'lucide-react'
 function App() {
-  const [count, setCount] = useState(0)
+  const {authUser, checkAuth, isCheckingAuth} = useAuthStore();
 
+  useEffect(()=>{
+    checkAuth();
+  }, [checkAuth])
+
+  console.log({authUser});
+
+  if(isCheckingAuth && !authUser){
+    return(
+      <div className='flex items-center justify-center h-screen'>
+        <Loader className='size-10 animate-spin' />
+      </div>
+    )
+  }
   return (
     <>
       <div className='text-amber-200'>
@@ -18,11 +33,11 @@ function App() {
         <Navbar />
 
         <Routes>
-          <Route path="/" element={<HomePage/>} />
-          <Route path="/signup" element={<SignUpPage/>} />
-          <Route path="/login" element={<LoginPage/>} />
+          <Route path="/" element={authUser ? <HomePage/> : <Navigate to="/login" />} />
+          <Route path="/signup" element={!authUser ? <SignUpPage/> : <Navigate to="/" />} />
+          <Route path="/login" element={!authUser ? <LoginPage/> : <Navigate to="/" />} />
           <Route path="/setting" element={<SettingsPage/>} />
-          <Route path="/profile" element={<ProfilePage/>} />
+          <Route path="/profile" element={authUser ? <ProfilePage/> : <Navigate to="/login" />} />
         </Routes>
       </div>
     </>
